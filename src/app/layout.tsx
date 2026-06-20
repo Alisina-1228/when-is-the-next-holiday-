11998ced63772937e8721951078b3a7dd6d62d84
Alisina Fakhraei <alisina.fakhraei@gmail.com>
2026-06-20 12:50:15 +0330

    feat: 👀 emoji favicon as SVG file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "تعطیلی بعدی چه روزیه؟ 👀",
   description: "تعطیلات آینده تقویم ایران",
-  icons: { icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>👀</text></svg>" },
+  icons: { icon: "/favicon.svg" },
 };
 
 export default function RootLayout({
